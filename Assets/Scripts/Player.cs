82bb6012bf74853ad2a268980ab0ae28e259da94
German Daniel Boso <german.boso@gmail.com>
2021-10-24 23:28:29 -0300

    uploading work in udemy course as I worked it on the wrong folder until looping spawn  of enemy waves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,81 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFirePeriod = 0.2f;
+
+    Coroutine fireCourutine;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
     // Start is called before the first frame update
     void Start()
     {
+        SetUpMoveBoundaries();
         
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
     }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fireCourutine = StartCoroutine(FireContinously());
+            
+            
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            //stops ALL coroutines
+            //StopAllCoroutines();
+
+            //stop selected
+            StopCoroutine(fireCourutine);
+        }
+    }
+
+    private IEnumerator FireContinously()
+    {
+        while (true) { 
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            Destroy(laser, 2f);
+            yield return new WaitForSeconds(projectileFirePeriod);
+        }
+    }
+
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
+        var newXPos = Mathf.Clamp( transform.position.x + deltaX, xMin, xMax );
+
+        var newYPos = Mathf.Clamp( transform.position.y + deltaY , yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+
+    }
+
+
 }
